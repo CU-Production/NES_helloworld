6787273997f9072468b86230cfff7580e2f09371
THISISAGOODNAME <yangyj19931231@gmail.com>
2025-06-03 14:42:54 +0800

    Add basic build scripts

---
+++ clear.bat
@@ -0,0 +1,10 @@
+@del example.o
+@del example.nes
+@del example.map.txt
+@del example.labels.txt
+@del example.nes.ram.nl
+@del example.nes.0.nl
+@del example.nes.1.nl
+@del example.nes.dbg
+@del example.nes.deb
+@echo.
